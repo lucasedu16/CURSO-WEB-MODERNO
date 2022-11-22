3d59905c20327cb359c1c03b2229e7ee1ec97d11
Lucas Eduardo <lucasdeusetudo80@gmail.com>
2022-11-22 14:03:50 -0300

    templateStrings

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -3,4 +3,4 @@ const escola = "cod3r"
 console.log(escola.charAt(4)) //imprime a letra selecionada no index
 console.log(escola.charAt(5))
 console.log(escola.substring(1)) //Percorre a string a partir de certo index
-console.log(escola.substring(1, 3)) //percorre a strig a partir do index 1 até o 3
+console.log(escola.substring(1, 3)) //percorre a strig a partir do index 1 até o 3 
---
+++ fundamentos/templateStrig.js
@@ -0,0 +1,11 @@
+const nome = "rebeca"
+const concatenacai = 'olá ' + nome + "!"
+const template = `
+Olá
+${nome}!
+`
+console.log(concatenacai, template)
+
+//espressoes
+
+console.log(`1 + 1 = ${1 + 1}`)
